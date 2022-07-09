44987831b0293a189ca8a062086e2ce9faeb286d
mike <michael.p9392@gmail.com>
2022-07-09 21:07:46 +0300

    master reverted

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    print("sample project for git testing")
+    print("sample project for git testing.")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
